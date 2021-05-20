9bdab4bb008fb74a9ab748aa4566ac55b44db431
Daniel Ramirez <daniel_r129@hotmail.com>
2021-05-20 15:10:45 -0700

    Functional TicTacToe game

--- app/src/main/java/com/example/tictactoe/MainActivity.java
+++ app/src/main/java/com/example/tictactoe/MainActivity.java
@@ -13,7 +13,6 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,15 +20,31 @@ public class MainActivity extends AppCompatActivity {
         switchPlayer();
         Button newGameButton = findViewById(R.id.newGame);
         newGameButton.setText("New Game");
+
+
     }
 
 
     public void onClick(View clicked) {
+        ArrayList<Button> buttonList = getButtonList();
+
         Button button = findViewById(clicked.getId());
         if(button.getText().length() == 0) {
             String player = determinePlayer();
             button.setText(player);
             switchPlayer();
+        }
+
+        if(isWon()) {
+            for(Button buttonDisable : buttonList) {
+                buttonDisable.setClickable(false);
+            }
+        }
+
+        else if(!isWon() && isTie()) {
+            for(Button buttonDisable : buttonList) {
+                buttonDisable.setClickable(false);
+            }
         }
     }
 
@@ -58,6 +73,21 @@ public class MainActivity extends AppCompatActivity {
     // Will try to refactor this method to involve a loop to find the button ids
     // for cleaner, shorter code
     public void newGame(View view) {
+        ArrayList<Button> buttonList = getButtonList();
+
+        for (Button button: buttonList) {
+            button.setText("");
+        }
+
+        for (Button button: buttonList) {
+            button.setClickable(true);
+        }
+
+        TextView turnText = findViewById(R.id.turnText);
+        turnText.setText("Player X's Turn");
+    }
+
+    private ArrayList<Button> getButtonList() {
         ArrayList<Button> buttonList = new ArrayList<>();
 
         buttonList.add(findViewById(R.id.ticTac1));
@@ -69,12 +99,80 @@ public class MainActivity extends AppCompatActivity {
         buttonList.add(findViewById(R.id.ticTac7));
         buttonList.add(findViewById(R.id.ticTac8));
         buttonList.add(findViewById(R.id.ticTac9));
+        return buttonList;
+    }
 
-        for (Button button: buttonList) {
-            button.setText("");
+    private boolean isWon() {
+        String[][] buttonArray = buttonsAsArray();
+
+        for(int row = 0; row < buttonArray.length; row++) {
+            if(buttonArray[row][0].equals(buttonArray[row][1]) &&
+                    buttonArray[row][0].equals(buttonArray[row][2]) &&
+                    !buttonArray[row][0].isEmpty()) {
+                setWinner(buttonArray[row][0]);
+                return true;
+            }
         }
 
-        TextView turnText = findViewById(R.id.turnText);
-        turnText.setText("Player X's Turn");
+        for(int column = 0; column < buttonArray[0].length; column++) {
+            if(buttonArray[0][column].equals(buttonArray[1][column]) &&
+                    buttonArray[0][column].equals(buttonArray[2][column]) &&
+                    !buttonArray[0][column].isEmpty()) {
+                setWinner(buttonArray[0][column]);
+                return true;
+            }
+        }
+
+        if(buttonArray[0][0].equals(buttonArray[1][1]) &&
+                buttonArray[1][1].equals(buttonArray[2][2]) &&
+                !buttonArray[0][0].isEmpty()) {
+            setWinner(buttonArray[0][0]);
+            return true;
+        }
+
+        if(buttonArray[2][0].equals(buttonArray[1][1]) &&
+                buttonArray[1][1].equals(buttonArray[0][2]) &&
+                !buttonArray[2][0].isEmpty()) {
+            setWinner(buttonArray[2][0]);
+            return true;
+        }
+
+        return false;
+    }
+
+    private boolean isTie() {
+        boolean isTieGame = true;
+        ArrayList<Button> buttonList = getButtonList();
+
+        for(Button button : buttonList) {
+            if(button.getText().toString().isEmpty()) {
+                isTieGame = false;
+            }
+        }
+
+        if(isTieGame) {
+            TextView winnerText = findViewById(R.id.turnText);
+            winnerText.setText("Game ended in a draw");
+        }
+        return isTieGame;
+    }
+
+    private String[][] buttonsAsArray() {
+        ArrayList<Button> buttonList = getButtonList();
+
+        String[][] buttonArray = new String[3][3];
+        int index = 0;
+        for(int row = 0; row < buttonArray.length; row++) {
+            for(int column = 0; column < buttonArray[row].length; column++) {
+                buttonArray[row][column] = buttonList.get(index).getText().toString();
+                index++;
+            }
+        }
+        return buttonArray;
+    }
+
+    private void setWinner(String winner) {
+        TextView winnerText = findViewById(R.id.turnText);
+        winnerText.setText("Player " + winner + " wins!");
     }
 }
